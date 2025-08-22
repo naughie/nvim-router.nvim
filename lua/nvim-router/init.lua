a37f66b1bfcb1780bfe9a4113ca261690f07ad6e
naughie <masaton@naughie.com>
2025-08-22 21:22:24 +0900

    Refactor and improve efficiency

--- lua/nvim-router/init.lua
+++ lua/nvim-router/init.lua
@@ -1,175 +1,72 @@
 local M = {}
 
-local function mkdir(dir, callback)
-    local mode = tonumber("755", 8)
-    vim.uv.fs_stat(dir, function(err, stat)
-        if stat and stat.type == 'directory' then
+local fs_struct = require("nvim-router.fs_struct")
+local gen = require("nvim-router.gen")
+
+local job_state = { id = nil }
+
+local function create_bin_project(plugin_dir, deps, callback)
+    gen.root_dir(plugin_dir, function()
+        if not gen.cargo_toml(plugin_dir, deps) then return end
+
+        gen.src_dir(plugin_dir, function()
+            if not gen.main_rs(plugin_dir, deps) then return end
+
             callback()
+        end)
+    end)
+end
+
+local function executable(plugin_dir)
+    local cmd = fs_struct.binary(plugin_dir)
+    return vim.fn.executable(cmd) == 1
+end
+
+local function spawn(plugin_dir)
+    if job_state.id then return end
+    if not executable(plugin_dir) then return end
+
+    local cmd = fs_struct.binary(plugin_dir)
+    local id = vim.fn.jobstart({ cmd }, { rpc = true })
+    if id ~= 0 and id ~= 1 then
+        job_state.id = id
+
+        return true
+    end
+end
+
+local function kill()
+    if not job_state.id then return end
+    return vim.fn.jobstop(job_state.id)
+end
+
+local function build_and_spawn(plugin_dir, deps, force)
+    local new_deps = gen.last_deps.content(deps)
+
+    if not force then
+        local last_deps = gen.last_deps.read(plugin_dir)
+
+        if last_deps == new_deps and executable(plugin_dir) then
+            spawn(plugin_dir)
+            return
         end
-        vim.uv.fs_mkdir(dir, mode, function(err)
-            if err then return end
-            callback()
+    end
+
+    kill()
+    create_bin_project(plugin_dir, deps, function()
+        vim.system({ "cargo", "build", "--release" }, { cwd = fs_struct.root_dir(plugin_dir) }, function(out)
+            if out.code == 0 then
+                gen.last_deps.write(plugin_dir, new_deps)
+                vim.schedule(function()
+                    spawn(plugin_dir)
+                end)
+            end
         end)
-    end)
-end
-
-local function gen_cargo(root_dir, deps)
-    local content = [[
-        [package]
-        name = "nvim-router"
-        version = "0.1.0"
-        edition = "2024"
-
-        [dependencies]
-        nvim-router = { git = "https://github.com/naughie/nvim-router.rs.git", branch = "main", features = ["tokio"] }
-        tokio = { version = "1", features = ["macros", "rt-multi-thread", "sync"] }
-    ]]
-
-    for i, dep in ipairs(deps) do
-        local dep_line = string.format('dep%d = { package = "%s", path = "%s" }', i, dep.package, dep.path)
-        content = content .. dep_line .. "\n"
-    end
-
-    local cargo_path = root_dir .. "/Cargo.toml"
-    local file = io.open(cargo_path, "w")
-    if not file then return end
-
-    local write_ok = file:write(content)
-    if not write_ok  then return end
-
-    local close_ok = file:close()
-    if not close_ok then return end
-
-    return true
-end
-
-local function gen_main(root_dir, deps)
-    local body = [[
-        use std::error::Error;
-
-        use tokio::fs::File as TokioFile;
-
-        use nvim_router::NeovimHandler;
-        use nvim_router::Router;
-        use nvim_router::nvim_rs::{compat::tokio::Compat, create::tokio as create};
-
-        type NvimWtr = Compat<TokioFile>;
-    ]]
-
-    for i, dep in ipairs(deps) do
-        local dep_mod = string.format([[
-            mod dep%d {
-                pub use dep%d::%s as H;
-                #[derive(Clone)]
-                pub struct N;
-                impl nvim_router::Namespace for N {
-                    const NS: &str = "%s";
-                }
-            }
-        ]], i, i, dep.handler, dep.ns)
-        body = body .. dep_mod
-    end
-
-    local deps_types = ""
-    for i in ipairs(deps) do
-        deps_types = deps_types .. string.format("(dep%d::N, dep%d::H),", i, i)
-    end
-
-    local deps_vals = ""
-    for i in ipairs(deps) do
-        deps_vals = deps_vals .. string.format("(dep%d::N, <dep%d::H as NeovimHandler<NvimWtr>>::new()),", i, i)
-    end
-
-    body = body .. [[
-    ]]
-
-    body = body .. string.format([[
-        #[tokio::main]
-        async fn main() -> Result<(), Box<dyn Error>> {
-            let handler = Router::<NvimWtr, (%s)>::new((%s));
-            let (nvim, io_handler) = create::new_parent(handler).await?;
-
-            // Any error should probably be logged, as stderr is not visible to users.
-            match io_handler.await {
-                Err(joinerr) => eprintln!("Error joining IO loop: '{joinerr}'"),
-                Ok(Err(err)) => {
-                    if !err.is_reader_error() {
-                        // One last try, since there wasn't an error with writing to the
-                        // stream
-                        nvim.err_writeln(&format!("Error: '{err}'"))
-                            .await
-                            .unwrap_or_else(|e| {
-                                // We could inspect this error to see what was happening, and
-                                // maybe retry, but at this point it's probably best
-                                // to assume the worst and print a friendly and
-                                // supportive message to our users
-                                eprintln!("Well, dang... '{e}'");
-                            });
-                    }
-
-                    if !err.is_channel_closed() {
-                        // Closed channel usually means neovim quit itself, or this plugin was
-                        // told to quit by closing the channel, so it's not always an error
-                        // condition.
-                        eprintln!("Error: '{err}'");
-
-                        let mut source = err.source();
-
-                        while let Some(e) = source {
-                            eprintln!("Caused by: '{e}'");
-                            source = e.source();
-                        }
-                    }
-                }
-                Ok(Ok(())) => {}
-            }
-
-            Ok(())
-        }
-    ]], deps_types, deps_vals)
-
-    local main_path = root_dir .. "/src/main.rs"
-    local file = io.open(main_path, "w")
-    if not file then return end
-
-    local write_ok = file:write(body)
-    if not write_ok  then return end
-
-    local close_ok = file:close()
-    if not close_ok then return end
-
-    return true
-end
-
-local function create_bin_project(root_dir, deps, callback)
-    local mode = tonumber("755", 8)
-    mkdir(root_dir, function()
-        if not gen_cargo(root_dir, deps) then return end
-
-        mkdir(root_dir .. "/src", function()
-            if not gen_main(root_dir, deps) then return end
-
-            callback()
-        end)
-    end)
-end
-
-local function create_and_build(root_dir, deps, callback)
-    create_bin_project(root_dir, deps, function()
-        vim.system({ "cargo", "build", "--release" }, { cwd = root_dir }, callback)
     end)
 end
 
 local opts = { deps = {} }
 local configured_deps = {}
-
-local main_dir = "/nvim-router-bin"
-
-local job_state = { id = nil }
-
-local function plugin_cmd()
-    return opts.plugin_dir .. main_dir .. "/target/release/nvim-router"
-end
 
 M.rpc = {
     notify = function(ns, name, ...)
@@ -187,31 +84,17 @@ M.rpc = {
     end,
 }
 
-function M.spawn()
-    if job_state.id or not opts.plugin_dir then return end
-    local cmd = plugin_cmd()
-
-    if vim.fn.executable(cmd) == 0 then return end
-
-    local id = vim.fn.jobstart({ cmd }, { rpc = true })
-    if id ~= 0 and id ~= 1 then
-        job_state.id = id
-
-        return true
-    end
+function M.build_and_spawn(force)
+    if not opts.plugin_dir or not opts.deps then return end
+    build_and_spawn(opts.plugin_dir, opts.deps, opts.force or force)
 end
 
 function M.build_if_all_registered()
-    if not opts.plugin_dir then return end
-
     for _, val in pairs(configured_deps) do
         if not val then return end
     end
 
-    local root_dir = opts.plugin_dir .. main_dir
-    create_and_build(root_dir, opts.deps, function()
-        vim.schedule(M.spawn)
-    end)
+    M.build_and_spawn()
 end
 
 function M.register(dep)
@@ -223,6 +106,7 @@ end
 
 function M.setup(init_opts)
     opts.plugin_dir = init_opts.plugin_dir
+    opts.force = init_opts.force
 
     for _, ns in ipairs(init_opts.ns) do
         if not configured_deps[ns] then 
